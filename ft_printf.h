25f1a662ca6284d2b4b0895f3f3fb9b75ec763b5
ponter <88228447+damiatrbl@users.noreply.github.com>
2024-12-27 17:05:19 +0800

    Create ft_printf.h

---
+++ ft_printf.h
@@ -0,0 +1,14 @@
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+# include <limits.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int    ft_printf(const char *a, ...);
+int    ft_putchar(int c);
+
+#endif
